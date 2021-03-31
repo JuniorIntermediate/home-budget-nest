e10901634f366ea2b8a3d2da360e2a7fae9ece7f
Mikolaj Szymanski <mikszym95@gmail.com>
2021-03-31 23:02:23 +0200

    Add expense module.

--- src/core/schema-types/expense.params.ts
+++ src/core/schema-types/expense.params.ts
@@ -1,17 +1,3 @@
 import { Prisma } from '@prisma/client';
 
 export type ExpenseCreateParams = Prisma.ExpenseCreateInput
-
-export type ExpenseUpdateParams = {
-  where: Prisma.ExpenseWhereUniqueInput,
-  data: Prisma.ExpenseUpdateInput
-}
-
-export type ExpenseGetParams = {
-  skip?: number,
-  take?: number,
-  where?: Prisma.ExpenseWhereInput,
-  orderBy?: Prisma.ExpenseOrderByInput
-}
-
-export type ExpenseGetByUniqueFieldParams = Prisma.ExpenseWhereUniqueInput
